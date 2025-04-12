76033bd30a59ca61c06a02457ddc594858e46a55
iza <126572391+izabellealencar05@users.noreply.github.com>
2025-04-12 15:37:08 -0300

    Create TerceiraTela.java

---
+++ MiniProva2/TerceiraTela.java
@@ -0,0 +1,47 @@
+package com.example.treinandoparaprova2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TerceiraTela extends AppCompatActivity {
+    private TextView resultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_terceira_tela);
+
+        inicializaTerceiraTela();
+    }
+    private void inicializaTerceiraTela() {
+        resultado = findViewById(R.id.terceiraTela_resultado);
+        int notaFinal = calculaNota();
+        String nome = getIntent().getStringExtra("chaveNome");
+        String matricula = getIntent().getStringExtra("chaveMatricula");
+
+        if (notaFinal == 10) {
+            resultado.setText("Parabéns, " + nome + " (" + matricula + ")\nVocê tirou 10 e foi aprovado(a)!");
+        } else if (notaFinal == 5) {
+            resultado.setText(nome + " (" + matricula + ")\nVocê tirou 5, mas foi aprovado(a)!");
+        } else {
+            resultado.setText(nome + " (" + matricula + ")\nInfelizmente, você foi reprovado(a).");
+        }
+
+    }
+    private int calculaNota() {
+        String resposta1 = getIntent().getStringExtra("chaveResposta1");
+        String resposta2 = getIntent().getStringExtra("chaveResposta2");
+
+        if (resposta1.equals("brasilia") && resposta2.equals("roxo"))
+            return 10;
+        else if (resposta1.equals("brasilia") || resposta2.equals("roxo"))
+            return 5;
+        return 0;
+    }
+}
