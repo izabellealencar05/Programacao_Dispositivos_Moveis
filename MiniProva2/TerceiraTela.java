f3eb07abc65233aa9f1065a767cab12c34f4993e
iza <126572391+izabellealencar05@users.noreply.github.com>
2025-04-14 18:23:33 -0300

    Update TerceiraTela.java

--- MiniProva2/TerceiraTela.java
+++ MiniProva2/TerceiraTela.java
@@ -17,14 +17,19 @@ public class TerceiraTela extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_terceira_tela);
 
-        inicializaTerceiraTela();
+        inicializaTerceiraTela(); //inicia terceira tela
     }
     private void inicializaTerceiraTela() {
+        //conecta o campo com a variavel criada
         resultado = findViewById(R.id.terceiraTela_resultado);
+        //cria a variavel de nota, e chama o metodo para calcular a nota
         int notaFinal = calculaNota();
+
+        //recupera os dados da pagina anterior
         String nome = getIntent().getStringExtra("chaveNome");
         String matricula = getIntent().getStringExtra("chaveMatricula");
 
+        //verifica a nota final do usuario e exibe uma mensagem de acordo com a nota
         if (notaFinal == 10) {
             resultado.setText("Parabéns, " + nome + " (" + matricula + ")\nVocê tirou 10 e foi aprovado(a)!");
         } else if (notaFinal == 5) {
@@ -32,12 +37,14 @@ public class TerceiraTela extends AppCompatActivity {
         } else {
             resultado.setText(nome + " (" + matricula + ")\nInfelizmente, você foi reprovado(a).");
         }
-
     }
+    //chama o metodo para calcular a nota
     private int calculaNota() {
+        //recupera os dados das respostas da pagina anterior
         String resposta1 = getIntent().getStringExtra("chaveResposta1");
         String resposta2 = getIntent().getStringExtra("chaveResposta2");
 
+        //faz a validacao das respostas
         if (resposta1.equals("brasilia") && resposta2.equals("roxo"))
             return 10;
         else if (resposta1.equals("brasilia") || resposta2.equals("roxo"))
