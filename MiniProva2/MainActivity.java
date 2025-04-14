92109ef4bd7ea6aca31100a95705d5e0e72a2c43
iza <126572391+izabellealencar05@users.noreply.github.com>
2025-04-14 18:22:51 -0300

    Update MainActivity.java

--- MiniProva2/MainActivity.java
+++ MiniProva2/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //declarando as variaveis
     private EditText nomeAluno;
     private EditText matriculaAluno;
     private Button botaoIniciar;
@@ -22,20 +23,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        inicializarTela();
+        inicializarTela(); //criando o metodo para inicializar a tela
     }
 
     private void inicializarTela() {
+        //conectar os campos das variaveis declaradas la em cima
         nomeAluno = findViewById(R.id.mainActivity_nome);
         matriculaAluno = findViewById(R.id.mainActivity_matricula);
         botaoIniciar = findViewById(R.id.mainActivity_botao);
 
+        //dando uma funcionalidade para o botao de iniciar
         botaoIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //pegando o que foi escrito nos campos (nome e matricula) e transformando para string
                 String nome = nomeAluno.getText().toString();
                 String matricula = matriculaAluno.getText().toString();
 
+                //se um campo estiver vazio aparece a mensagem
                 if (nome.isEmpty() || matricula.isEmpty()){
                     Toast.makeText(MainActivity.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
                 } else {
@@ -45,16 +50,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //criando metodo para chamar a proxima tela
     private void passarProxTela() {
+        //criando o intent para ir para a proxima tela,
+        // This - tela de origem
+        // SegundaTela.class - tela final
         Intent proxTela = new Intent(this, SegundaTela.class);
 
+        //adicionando no intent chave para passar os dados para a proxima tela
         proxTela.putExtra("chaveNome", nomeAluno.getText().toString());
         proxTela.putExtra("chaveMatricula", matriculaAluno.getText().toString());
 
-
+        //inicia a proxima tela
         startActivity(proxTela);
 
     }
-
 }
