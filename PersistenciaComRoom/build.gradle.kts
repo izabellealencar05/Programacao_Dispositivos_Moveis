1f94bebb58d22f0f200b876bedd16c7df7da9295
iza <126572391+izabellealencar05@users.noreply.github.com>
2025-05-06 09:44:19 -0300

    Create build.gradle.kts

---
+++ PersistenciaComRoom/build.gradle.kts
@@ -0,0 +1,48 @@
+//build.gradle.kts (module:app)
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.persistenciacomroom"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.persistenciacomroom"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    //implementação do codigo, apertar no 'sync now'
+    implementation("androidx.room:room-runtime:2.7.1")
+    annotationProcessor("androidx.room:room-compiler:2.7.1")
+}
